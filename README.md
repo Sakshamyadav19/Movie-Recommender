b490d02d106de2e5b78b7cfe94b26d657d3d4173
Ashutosh Muley <muleyashutosh@gmail.com>
2020-05-29 22:30:47 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Movie Recommender
+
+A Mini Project with the base purpose to provide recomemdations, imDB ratings, details about the movie you desire...
---
+++ movierecommender.py
@@ -0,0 +1,29 @@
+import requests
+import json
+def recommend(movieName):
+    baseurl='https://tastedive.com/api/similar'
+    params_d = {}
+    params_d['q']= movieName
+    params_d['k']='373171-Sam-UW1YNP5R'
+    params_d['type']= 'movies'
+    params_d['limit'] = '5'
+    resp = requests.get(baseurl, params=params_d)
+    data=json.loads(resp.text)
+    for x in range(0,5):
+      print(data['Similar']['Results'][x]['Name'])
+def info(name):
+  url='http://www.omdbapi.com/'
+  par={}
+  par['apikey']='d817e6ac'
+  par['t']=name
+  resp=requests.get(url,params=par)
+  data=json.loads(resp.text)
+  print('imdbRating:',data['imdbRating'])
+  print('Awards:',data['Awards'])
+  print('Plot:',data['Plot'])
+
+movie=input('Enter Movie:')
+info(movie)
+print('===========')
+print('Recommedations')
+recommend(movie)
